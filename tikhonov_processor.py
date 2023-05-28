3107088c0ed2ac6361f7cd126d444836ae193831
Dm1stry <BattleshipWriter@yandex.ru>
2023-05-28 13:41:55 +0300

    Tikhonov Processor filled up to working state

--- tikhonov_processor.py
+++ tikhonov_processor.py
@@ -1,4 +1,5 @@
 from PyQt6.QtCore import QObject
+import numpy as np
 
 
 class Params:
@@ -15,20 +16,51 @@ class Params:
         self.alpha = alpha
         self.p_size = p_size
 
+class Results:
+    t = None
+    A = None
+    pt = None
+    p = None
 
 class TikhonovProcessor(QObject):
     def __init__(self):
         super().__init__()
         self.params = None
+        self.results = None
 
-    def setParams(self, params):
+    def setParams(self, params=Params()):
         self.params = params
 
     def Process(self, data):
-        pass
+        p = np.logspace(np.log10(1 / self.params.T_max), np.log10(1 / self.params.T_min), self.params.p_size)
+        t = data[:, 0]
+        s = data[:, 1]
+        pp, tt = np.meshgrid(p, t)
+        K = np.exp(-pp * tt)
+
+        r = self.__regularizate(K, np.zeros(self.params.p_size), s, self.params.alpha, self.params.iterations)
+
+        self.results = Results()
+        self.results.t = t
+        self.results.A = K @ r
+        self.results.pt = 1/p
+        self.results.p = r
+
+    def __regularigation(self, K, r, s, alfa, iterations):
+        K_t = np.ascontiguousarray(np.transpose(K))
+
+        W = np.ascontiguousarray(np.linalg.inv(K_t @ K + alfa * np.eye(r.size)))
+        s = np.ascontiguousarray(s)
+        K_t_s = np.ascontiguousarray(K_t @ s)
+
+        for i in range(iterations):
+            r = W @ (K_t_s + alfa * r)
+            r[r < 0] = 0
+
+        return r
 
     def getSpectrum(self):
-        pass
+        return self.results.pt, self.results.p
 
     def getCurve(self):
-        pass
+        return self.results.t, self.results.A
